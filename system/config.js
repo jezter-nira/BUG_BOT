49b623b1654c22d7d3ea307f149b669991d63828
jezter-nira <charithniroshana7@gmail.com>
2025-07-26 04:08:33 -0600

    Create config.js

---
+++ system/config.js
@@ -0,0 +1,12 @@
+//========HELO FRIEND========//
+global.prefix = [".", "!", ".", ",", "🐤", "🗿"]; 
+global.publik = true
+global.owner = ["6281931658034"] 
+global.namabot = 'CRASHER INVISIBLE'
+//======================
+global.mess = { 
+owner: '*waduhh!, lu bukan owner gw bg*',
+premium: '*anda bukan user premium*',
+succes: '*done bang*'
+}
+//======================
